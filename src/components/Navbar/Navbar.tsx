fa720b7aa6817f38ad7f5dbb90f547df0522cafd
spiderthatcodes <75698869+jdickerson-banyan@users.noreply.github.com>
2023-06-16 09:16:52 -0700

    coding the nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react';
-import { NavContainer } from './style'
+import { useLocation } from 'react-router-dom';
+import { NavContainer, LayerOne, LayerTwo, LayerThree } from './style';
 
 const Navbar: FC = () => {
-  return (
-    <NavContainer>
-        {/* layer one */}
-        {/* layer two */}
-        {/* layer three, not visible on home page */}
-    </NavContainer>
-  )
-}
+    const { pathname } = useLocation();
 
-export default Navbar
+
+    return (
+        <NavContainer>
+            <LayerOne>
+                <h1 id='logo'><span>Phoenix</span>Public<span>Library</span></h1>
+                <div>
+                    <button className='orange-button'>Translate</button>
+                    <button className='orange-button'>My Account</button>
+                </div>
+            </LayerOne>
+            <LayerTwo></LayerTwo>
+            {pathname !== '/' && <LayerThree></LayerThree>}
+        </NavContainer>
+    );
+};
+
+export default Navbar;
--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -2,6 +2,40 @@ import styled from "styled-components";
 
 export const NavContainer = styled.div`
     position: sticky;
-    background-color: #17a2b8;
+    /* background-color: #17a2b8; */
 `;
 
+export const LayerOne = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 8px;
+
+    #logo {
+        margin: 0;
+        padding: 0;
+        font-size: 1.5rem;
+        font-weight: 400;
+    }
+
+    span {
+        font-weight: 900 !important;
+    }
+
+    .orange-button {
+        background-color: #e25300;
+        color: #fff;
+        border: none;
+        margin: 2px;
+        line-height: 1.5;
+        font-size: 1rem;
+        padding: 5px 10px;
+        font-weight: 400 !important;
+        border-radius: 4px;
+    }
+
+`;
+
+export const LayerTwo = styled.div``;
+
+export const LayerThree = styled.div``;
